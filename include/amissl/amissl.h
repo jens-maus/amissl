ed6b9d77a91353424c13c2bef54474929312ced3
Andrija Antonijevic <antony@heightanxiety.com>
2003-03-25 06:32:19 +0000

    Updated with new includes, removed RSA_PKCS1_SSLeay and RSA_PKCS1_RSAref defines.

--- include/amissl/amissl.h
+++ include/amissl/amissl.h
@@ -1,7 +1,11 @@
 #ifndef AMISSL_AMISSL_H
 #define AMISSL_AMISSL_H
 
+#include <amissl/amisslconf.h>
+#include <amissl/aes.h>
 #include <amissl/asn1.h>
+#include <amissl/asn1_mac.h>
+#include <amissl/asn1t.h>
 #include <amissl/bio.h>
 #include <amissl/blowfish.h>
 #include <amissl/bn.h>
@@ -12,20 +16,31 @@
 #include <amissl/conf_api.h>
 #include <amissl/crypto.h>
 #include <amissl/des.h>
+#include <amissl/des_old.h>
 #include <amissl/dh.h>
 #include <amissl/dsa.h>
 #include <amissl/dso.h>
+#include <amissl/ebcdic.h>
+#include <amissl/ec.h>
+/* #include <amissl/engine.h> */
 #include <amissl/err.h>
 #include <amissl/evp.h>
 #include <amissl/hmac.h>
 #include <amissl/idea.h>
+#include <amissl/krb5_asn.h>
+#include <amissl/kssl.h>
 #include <amissl/lhash.h>
 #include <amissl/md2.h>
 #include <amissl/md4.h>
 #include <amissl/md5.h>
 #include <amissl/mdc2.h>
+#include <amissl/obj_mac.h>
 #include <amissl/objects.h>
+#include <amissl/ocsp.h>
+#include <amissl/opensslv.h>
+#include <amissl/ossl_typ.h>
 #include <amissl/pem.h>
+#include <amissl/pem2.h>
 #include <amissl/pkcs12.h>
 #include <amissl/pkcs7.h>
 #include <amissl/rand.h>
@@ -33,19 +48,24 @@
 #include <amissl/rc4.h>
 #include <amissl/rc5.h>
 #include <amissl/ripemd.h>
-#include <amissl/rsaref.h>
 #include <amissl/rsa.h>
+#include <amissl/safestack.h>
 #include <amissl/sha.h>
 #include <amissl/ssl.h>
+#include <amissl/ssl2.h>
+#include <amissl/ssl23.h>
+#include <amissl/ssl3.h>
 #include <amissl/stack.h>
+#include <amissl/symhacks.h>
+#include <amissl/tls1.h>
+#include <amissl/tmdiff.h>
 #include <amissl/txt_db.h>
-#include <amissl/x509v3.h>
+#include <amissl/ui.h>
+#include <amissl/ui_compat.h>
 #include <amissl/x509.h>
 #include <amissl/x509_vfy.h>
+#include <amissl/x509v3.h>
 
 #include <amissl/tags.h>
-
-#define RSA_PKCS1_SSLeay RSA_PKCS1_Default
-#define RSA_PKCS1_RSAref RSA_PKCS1_Default
 
 #endif /* AMISSL_AMISSL_H */
