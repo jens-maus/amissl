221f2e459a0a6122e0b5574e2ec6ad65bbf23e91
Andrija Antonijevic <antony@heightanxiety.com>
2005-01-08 04:10:23 +0000

    OS4 changes.

--- include/proto/amisslmaster.h
+++ include/proto/amisslmaster.h
@@ -4,14 +4,45 @@
 #ifndef EXEC_TYPES_H
 #include <exec/types.h>
 #endif
-#ifndef CLIB_AMISSLMASTER_PROTOS_H
-#include <clib/amisslmaster_protos.h>
-#endif
+
+/****************************************************************************/
 
 #ifndef __NOLIBBASE__
 extern struct Library *AmiSSLMasterBase;
-#endif
+#endif /* __NOLIBBASE__ */
 
-#include <pragmas/amisslmaster_pragmas.h>
+/****************************************************************************/
+
+#ifdef __amigaos4__
+ #include <interfaces/amisslmaster.h>
+ #ifdef __USE_INLINE__
+  #include <inline4/amisslmaster.h>
+ #endif /* __USE_INLINE__ */
+ #ifndef CLIB_AMISSLMASTER_PROTOS_H
+  #define CLIB_AMISSLMASTER_PROTOS_H 1
+ #endif /* CLIB_AMISSLMASTER_PROTOS_H */
+ #ifndef __NOGLOBALIFACE__
+  extern struct AmiSSLMasterIFace *IAmiSSLMaster;
+ #endif /* __NOGLOBALIFACE__ */
+#else /* __amigaos4__ */
+ #ifndef CLIB_AMISSLMASTER_PROTOS_H
+  #include <clib/amisslmaster_protos.h>
+ #endif /* CLIB_AMISSLMASTER_PROTOS_H */
+ #if defined(__GNUC__)
+  #ifndef __PPC__
+   #include <inline/amisslmaster.h>
+  #else
+   #include <ppcinline/amisslmaster.h>
+  #endif /* __PPC__ */
+ #elif defined(__VBCC__)
+  #ifndef __PPC__
+   #include <inline/amisslmaster_protos.h>
+  #endif /* __PPC__ */
+ #else
+  #include <pragmas/amisslmaster_pragmas.h>
+ #endif /* __GNUC__ */
+#endif /* __amigaos4__ */
+
+/****************************************************************************/
 
 #endif /* PROTO_AMISSLMASTER_H */
