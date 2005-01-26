eae5cdc76124a3058fc515453f562c33c728c4a1
Andrija Antonijevic <antony@heightanxiety.com>
2005-01-26 08:06:06 +0000

    Commited the version from OS4 aware SDK for AmiSSL v1.1.

--- test/https.c
+++ test/https.c
@@ -5,148 +5,159 @@
 #include <netdb.h>
 #include <dos/dos.h>
 
-#define __USE_SYSBASE	1
-
 #include <proto/exec.h>
 #include <proto/dos.h>
-#include <proto/amisslmaster.h>
 #include <proto/amissl.h>
-#include <libraries/amisslmaster.h>
-#include <multitcp.h>
+#include <proto/socket.h>
+#include <amissl/amissl.h>
 
-LONG Init(void);
+BOOL Init(void);
 void Cleanup(void);
-int do_connect(char *, short , char *, short );
-BPTR getstderr(void);
-void MiniSPrintf(char *, char *, ...);
+int ConnectToServer(char *, short, char *, short);
 
-struct Library *AmiSSLBase = NULL;
-struct Library *AmiSSLMasterBase = NULL;
-APTR stack = NULL;
+struct Library *AmiSSLBase;
 
-/* four arguments: first is host *in IP format* and the second is port
- * to connect to. Third is proxy in IP format and the fourth proxy port.
+#ifdef __amigaos4__
+struct AmiSSLIFace *IAmiSSL;
+#else /* !__amigaos4__ */
+static BPTR ErrorOutput(void)
+{
+	return(((struct Process *)FindTask(NULL))->pr_CES);
+}
+#endif /* __amigaos4__ */
+
+static BPTR GetStdErr(void)
+{
+	BPTR err = ErrorOutput();
+
+	return(err ? err : Output());
+}
+
+/* The program expects at most four arguments: host in IP format, port
+ * number to connect to, proxy in IP format and proxy port number.
  * If last two are specified, host can be in any format proxy will
- * understand (yes I was lazy to write resolve code :))
+ * understand (since this is an example for SSL programming, host name
+ * resolving code is left out).
+ *
  * Default values are "127.0.0.1", 443. If any proxy parameter is
  * omitted program will connect directly to the host.
  */
-
-void main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int err = 0;
-	int sd;
-	SSL_CTX *ctx;
-	SSL *ssl;
+	const char rand[] = "fklgnlnhrjxnc 94ut c,mnxclnvhepgs poaw u5-wxfjl vfnkl";
+	char buffer[4096]; /* This should be dynamically allocated */
+	const char *request = "GET / HTTP/1.0\r\n\r\n";
+	BOOL is_ok = FALSE;
 	X509 *server_cert;
-	char *str;
-	char buf[4096];
-	BIO *bio_err = NULL;
-	char *request = "GET / HTTP/1.0\r\n\r\n";
-	char rand[] = "fklgnlnhrjxnc 94ut c,mnxclnvhepgs poaw u5-wxfjl vfnkl";
+	SSL_CTX *ctx;
+	BIO *bio_err;
+	SSL *ssl;
 
-	if (!Init())
+	if (Init())
 	{
 		/* Basic intialization. Next few steps (up to SSL_new()) need
-		 * to be done only once per AmiSSL opener
+		 * to be done only once per AmiSSL opener.
 		 */
-		ERR_load_crypto_strings();
-		ERR_load_SSL_strings();
-		OpenSSL_add_all_algorithms();
+		SSLeay_add_ssl_algorithms();
+		SSL_load_error_strings();
 
 		/* Much better *random* data needed! */
 		RAND_seed(rand, sizeof(rand));
 
+		/* Note: BIO writing routines are prepared for NULL BIO handle */
 		if (bio_err = BIO_new(BIO_s_file()))
-			BIO_set_fp_amiga(bio_err, getstderr(), BIO_NOCLOSE | BIO_FP_TEXT);
+			BIO_set_fp_amiga(bio_err, GetStdErr(), BIO_NOCLOSE | BIO_FP_TEXT);
 
+		/* Get a new SSL context */
 		if (ctx = SSL_CTX_new(SSLv23_client_method()))
 		{
-			/* Basic certificate handling. Look into OpenSSL to see what more
-			 * can be done about them
+			/* Basic certificate handling. OpenSSL documentation has more
+			 * information on this.
 			 */
-			/* SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER | SSL_VERIFY_FAIL_IF_NO_PEER_CERT, NULL); */
-			/* Don't do the following, it's here just for testing! */
-			SSL_CTX_set_verify(ctx, SSL_VERIFY_NONE, NULL);
 			SSL_CTX_set_default_verify_paths(ctx);
+			SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER | SSL_VERIFY_FAIL_IF_NO_PEER_CERT, NULL);
 
-			/* This needs to be done once per socket */
-
+			/* The following needs to be done once per socket */
 			if (ssl = SSL_new(ctx))
 			{
-				/* Connecting part. Uses CONNECT method if connecting
-				 * through a proxy
-				 */
+				int sock;
 
-				if (argc > 4)	/* Proxy */
-					sd = do_connect(argv[1], atol(argv[2]), argv[3], atol(argv[4]));
-				else			/* Direct */
-					sd = do_connect(argv[1] ? argv[1] : "127.0.0.1", argc > 2 ? atol(argv[2]) : 443, NULL, 0);
+				/* Connect to the HTTPS server, directly or through a proxy */
+				if (argc > 4)
+					sock = ConnectToServer(argv[1], atol(argv[2]), argv[3], atol(argv[4]));
+				else
+					sock = ConnectToServer(argv[1] ? argv[1] : "127.0.0.1",
+					                       argc > 2 ? atol(argv[2]) : 443,
+					                       NULL, 0);
 
-				if (sd >= 0)
+				/* Check if connection was established */
+				if (sock >= 0)
 				{
-					/* Associate a socket with ssl structure */
-					SSL_set_fd(ssl, sd);
+					int ssl_err = 0;
 
-					/* SSL handshake and other things */
-					if ((err = SSL_connect(ssl)) >= 0)
+					/* Associate the socket with the ssl structure */
+					SSL_set_fd(ssl, sock);
+
+					/* Perform SSL handshake */
+					if ((ssl_err = SSL_connect(ssl)) >= 0)
 					{
 						Printf("SSL connection using %s\n", SSL_get_cipher(ssl));
 
-						/* Certificate checking. This is *very* basic */
+						/* Certificate checking. This example is *very* basic */
 						if (server_cert = SSL_get_peer_certificate(ssl))
 						{
+							char *str;
+
 							Printf("Server certificate:\n");
 
 							if (str = X509_NAME_oneline(X509_get_subject_name(server_cert), 0, 0))
 							{
 								Printf("\tSubject: %s\n", str);
-								OPENSSL_free(str);
+								AmiSSL_free(str);
 							}
 							else
-								Printf("Warning: Couldn't read subject name in certificate!\n");
+								Printf("Warning: couldn't read subject name in certificate!\n");
 
 							if (str = X509_NAME_oneline(X509_get_issuer_name(server_cert), 0, 0))
 							{
 								Printf("\tIssuer: %s\n", str);
-								OPENSSL_free(str);
+								AmiSSL_free(str);
 							}
 							else
-								Printf("Warning: Couldn't read issuer name in certificate!\n");
+								Printf("Warning: couldn't read issuer name in certificate!\n");
 
 							X509_free(server_cert);
 
-							/* Send HTTP request */
-							if ((err = SSL_write(ssl, request, strlen(request))) >= 0)
+							/* Send a HTTP request. Again, this is just
+							 * a very basic example.
+							 */
+							if ((ssl_err = SSL_write(ssl, request, strlen(request))) > 0)
 							{
 								/* Dump everything to output */
-								while ((err = SSL_read(ssl, buf, sizeof(buf) - 1)) > 0)
-									FWrite(Output(), buf, err, 1);
+								while ((ssl_err = SSL_read(ssl, buffer, sizeof(buffer) - 1)) > 0)
+									FWrite(Output(), buffer, ssl_err, 1);
 
-								/* If there were errors, shout */
-								if (err < 0)
-									ERR_print_errors(bio_err);
+								/* This is not entirely true, check
+								 * the SSL_read documentation
+								 */
+								is_ok = ssl_err == 0;
 							}
 							else
-							{
-								long err;
-								char *p;
-
-								err = ERR_get_error();
-								p = ERR_error_string(err, 0);
-								Printf("SSL error code=%ld, error=%s\n", err, p);
 								Printf("Couldn't write request!\n");
-							}
 						}
 						else
 							Printf("Couldn't get server certificate!\n");
 					}
 					else
+						Printf("Couldn't establish SSL connection!\n");
+
+					/* If there were errors, print them */
+					if (ssl_err < 0)
 						ERR_print_errors(bio_err);
 
-					/* Send SSL/TLS close_notify */
+					/* Send SSL close notification and close the socket */
 					SSL_shutdown(ssl);
-					MTCP_CloseSocket(sd);
+					CloseSocket(sock);
 				}
 				else
 					Printf("Couldn't connect to host!\n");
@@ -161,116 +172,123 @@ void main(int argc, char **argv)
 		else
 			Printf("Couldn't create new context!\n");
 
-		ERR_free_strings();
-
 		Cleanup();
 	}
+
+	return(is_ok ? RETURN_OK : RETURN_ERROR);
+}
+
+/* Open and initialize AmiSSL. SocketBase should be initialized either by
+ * startup code or by the program itself.
+ */
+BOOL Init(void)
+{
+	BOOL is_ok = FALSE;
+
+	if (!(AmiSSLBase = OpenLibrary("amissl.library", 1)))
+		Printf("Couldn't open amissl.library v1!\n");
+#ifdef __amigaos4__
+	else if (!(IAmiSSL = (struct AmiSSLIFace *)GetInterface(AmiSSLBase, "main", 1, NULL)))
+		Printf("Couldn't get AmiSSL interface!\n");
+#endif /* __amigaos4__ */
+	else if (InitAmiSSL(AmiSSL_Version, AmiSSL_CurrentVersion,
+	                    AmiSSL_Revision, AmiSSL_CurrentRevision,
+	                    AmiSSL_SocketBase, SocketBase,
+	                    TAG_DONE) != 0)
+		Printf("Couldn't initialize AmiSSL!\n");
 	else
+		is_ok = TRUE;
+
+	if (!is_ok)
 	{
-		/* The error string is not much helpful O:) */
-		Printf("Error during initialization (couldn't open TCP/IP stack or AmiSSL or whatever)!\n");
-		Cleanup();
+#ifdef __amigaos4__
+		DropInterface((struct Interface *)IAmiSSL), IAmiSSL = NULL;
+#endif /* __amigaos4__ */
+		CloseLibrary(AmiSSLBase), AmiSSLBase = NULL;
 	}
+
+	return(is_ok);
 }
 
-/* Demonstration only. Ideally, it would say what exactly went wrong */
-static LONG Init(void)
+void Cleanup(void)
 {
-	LONG err = 1; /* Default: error */
-
-	if ((stack = MTCP_InitTCPIP(NULL))
-	    && (AmiSSLMasterBase = OpenLibrary("amisslmaster.library", 1))
-	    && InitAmiSSLMaster(AMISSL_CURRENT_VERSION, TRUE)
-	    && (AmiSSLBase = OpenAmiSSL())
-	    && !InitAmiSSL(AmiSSL_TCPStack, stack, TAG_DONE))
-		err = 0;
-
-	return(err);
+	CleanupAmiSSL(TAG_DONE);
+#ifdef __amigaos4__
+	DropInterface((struct Interface *)IAmiSSL);
+#endif /* __amigaos4__ */
+	CloseLibrary(AmiSSLBase);
 }
 
-static void Cleanup(void)
+/* Connect to the specified server, either directly or through the specified
+ * proxy using HTTP CONNECT method.
+ */
+int ConnectToServer(char *host, short port, char *proxy, short pport)
 {
-	if (AmiSSLBase)
-	{
-		CleanupAmiSSL(TAG_DONE);
-		CloseAmiSSL();
-		AmiSSLBase = NULL;
-	}
+	struct sockaddr_in addr;
+	char buffer[1024]; /* This should be dynamically alocated */
+	BOOL is_ok = FALSE;
+	char *s1, *s2;
+	int sock;
 
-	if (AmiSSLMasterBase)
+	/* Create a socket and connect to the server */
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) >= 0)
 	{
-		CloseLibrary(AmiSSLMasterBase);
-		AmiSSLMasterBase = NULL;
-	}
-
-	if (stack)
-	{
-		MTCP_DoneTCPIP(stack);
-		stack = NULL;
-	}
-}
-
-int do_connect(char *host, short port, char *proxy, short pport)
-{
-	struct sockaddr_in sa;
-	char buf[1024], *s1, *s2; /* Buffer is on stack only because programmer is lazy, don't do that */
-	int sock, err;
-
-	/* Connecting to server. Demonstration only.
-	 * Kids, don't try this at home ;)
-	 */
-	if ((sock = MTCP_Socket(stack, FALSE)) >= 0)
-	{
-		memset(&sa, 0, sizeof(sa));
-		sa.sin_family = AF_INET;
+		memset(&addr, 0, sizeof(addr));
+		addr.sin_family = AF_INET;
 
 		if (proxy && pport)
 		{
-			sa.sin_addr.s_addr = MTCP_Inet_Addr(stack, proxy);
-			sa.sin_port = htons(pport);
+			addr.sin_addr.s_addr = inet_addr(proxy); /* This should be checked against INADDR_NONE */
+			addr.sin_port = htons(pport);
 		}
 		else
 		{
-			sa.sin_addr.s_addr = MTCP_Inet_Addr(stack, host);
-			sa.sin_port = htons(port);
+			addr.sin_addr.s_addr = inet_addr(host); /* This should be checked against INADDR_NONE */
+			addr.sin_port = htons(port);
 		}
 
-		if ((err = MTCP_Connect(sock, (struct sockaddr *)&sa)) >= 0)
+		if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) >= 0)
 		{
-			/* Proxy connection, SSL tunneling. We send CONNECT and
-			 * after everything's done, proceed as with direct HTTPS
-			 * connection
+			/* For proxy connection, use SSL tunneling. First issue a HTTP CONNECT
+			 * request and then proceed as with direct HTTPS connection.
 			 */
 			if (proxy && pport)
 			{
-				MiniSPrintf(buf, "CONNECT %s:%lu HTTP/1.0\r\n\r\n", host, port);
+				/* This should be using snprintf, but some compilers don't have it */
+				sprintf(buffer, "CONNECT %s:%ld HTTP/1.0\r\n\r\n",
+				        host, (long)port);
 
-				/* We hope that everything gets sent at once
-				 * In real code, loop until err == 0 (or < 0 if error)
+				/* If this was a real application, it would be necessary to loop
+				 * until everything is sent or an error occurrs, but here we
+				 * hope that everything gets sent at once.
 				 */
-				if ((err = MTCP_Send(sock, buf, strlen(buf), 0)) >= 0)
+				if (send(sock, buffer, strlen(buffer), 0) >= 0)
 				{
-					if ((err = MTCP_Recv(sock, buf, sizeof(buf) - 1, 0)) >= 0)
+					/* Again, some optimistic behaviour: HTTP response might not be
+					 * received with only one recv
+					 */
+					if (recv(sock, buffer, sizeof(buffer) - 1, 0) >= 0)
 					{
-						if ((s1 = strchr(buf, '\r')) || (s1 = strchr(buf, '\n')))
+						/* Assuming it was received, find the end of the line and cut it off */
+						if ((s1 = strchr(buffer, '\r')) || (s1 = strchr(buffer, '\n')))
 							*s1 = '\0';
 
-						Printf("Proxy returned: %s\n", buf);
+						Printf("Proxy returned: %s\n", buffer);
 
-						/* Response makes sense? */
-						if (!strncmp(buf, "HTTP/", 4) && (s1 = strchr(buf, ' ')) && (s2 = strchr(++s1, ' ')) && (s2 - s1 == 3))
+						/* Check if HTTP response makes sense */
+						if (strncmp(buffer, "HTTP/", 4) == 0
+						    && (s1 = strchr(buffer, ' '))
+						    && (s2 = strchr(++s1, ' '))
+						    && (s2 - s1 == 3))
 						{
-							if (atol(s1) != 200)
-							{
-								Printf("Proxy responce indicates error, exiting!\n");
-								err = -1;
-							}
+							/* Only accept HTTP 200 OK response */
+							if (atol(s1) == 200)
+								is_ok = TRUE;
+							else
+								Printf("Proxy responce indicates error!\n");
 						}
 						else
-						{
-							Printf("Amibigous proxy responce, exiting!\n");
-							err = -1;
-						}
+							Printf("Amibigous proxy responce!\n");
 					}
 					else
 						Printf("Couldn't get proxy response!\n");
@@ -278,26 +296,16 @@ int do_connect(char *host, short port, char *proxy, short pport)
 				else
 					Printf("Couldn't send request to proxy!\n");
 			}
+			else
+				is_ok = TRUE;
 		}
 
-		if (err < 0)
+		if (!is_ok)
 		{
-			MTCP_CloseSocket(sock);
+			CloseSocket(sock);
 			sock = -1;
 		}
 	}
 
 	return(sock);
-}
-
-BPTR getstderr(void)
-{
-	struct Process *proc = (struct Process *)FindTask(NULL);
-
-	return((BPTR)(proc->pr_CES ? proc->pr_CES : proc->pr_COS));
-}
-
-void MiniSPrintf(char *buffer, char *format, ...)
-{
-	RawDoFmt(format, &format + 1, (void *)"\x16\xC0\x4E\x75", buffer);
 }
