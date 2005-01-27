9c816af51431c788e3cae177c9a515326b9d1635
Stefan Burström <stefan@ibrowse-dev.net>
2005-01-27 22:35:36 +0000

    removed

--- src/debug.h
+++
@@ -1,8 +0,0 @@
-#ifdef __amigaos4__
-extern struct ExecIFace * __attribute__((force_no_baserel)) IExec;
-#define kprintf (IExec->DebugPrintF)
-#define traceline() IExec->DebugPrintF("%08x %s:%d\n",FindTask(NULL),__FILE__,__LINE__)
-#else /* !__amigaos4__ */
-static void kprintf(const char *dummy, ...) {}
-#define traceline() kprintf("%08x %s:%d\n",FindTask(NULL),__FILE__,__LINE__)
-#endif /* __amigaos4__ */
