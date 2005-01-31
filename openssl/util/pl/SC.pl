96433a6b12a8ec39df06e51a0243a32b71950373
Andrija Antonijevic <antony@heightanxiety.com>
2005-01-31 01:32:33 +0000

    *** empty log message ***

--- openssl/util/pl/SC.pl
+++ openssl/util/pl/SC.pl
@@ -11,22 +11,23 @@ $mkdir='MakeDir';
 # C compiler stuff
 $cc="sc";
 $cfile="CSRC ";
+$cflags="";
 # There's something strange with __bulitin_memcmp in SAS
-#$cflags="CPU=\$(CPU_TYPE) \$(ADD) STRSECT=CODE DEF __USE_SYSBASE=1 DEF __builtin_memcmp=memcmp DEF AMISSL DEF AMISSL_COMPILE DEF B_ENDIAN DEF THIRTY_TWO_BITS IGN=55+72+154+161+304+306 NOSTKCHK STRMERGE ABSFUNCPOINTER IDLEN=100 MATH=STANDARD IDIR NetInclude: IDIR AmiSSL:include DEBUG=LINE OPT OPTGO OPTPEEP OPTLOOP OPTINL OPTINLOCAL OPTDEP=4 OPTSCHED";
-$cflags="CPU=\$(CPU_TYPE) \$(ADD) STRSECT=CODE DEF __USE_SYSBASE=1 DEF __builtin_memcmp=memcmp DEF AMISSL DEF AMISSL_COMPILE DEF B_ENDIAN DEF THIRTY_TWO_BITS IGN=55+72+154+161+304+306 NOSTKCHK STRMERGE ABSFUNCPOINTER IDLEN=100 MATH=STANDARD IDIR NetInclude: IDIR AmiSSL:include DEBUG=LINE";
+$app_cflag="\$(ADD) CPU=\$(CPU_TYPE) DEF __USE_SYSBASE=1 DEF __builtin_memcmp=memcmp DEF AMISSL DEF B_ENDIAN DEF THIRTY_TWO_BITS DEF OPENSSL_NO_FP_API DEF OPENSSL_NO_ENGINE IGN=55+72+154+161+304+306 NOSTKCHK STRMERGE ABSFUNCPOINTER IDLEN=100 MATH=STANDARD IDIR NetInclude: IDIR AmiSSL:include IDIR AmiSSL:openssl SAVEDS DEBUG=LINE";
+$lib_cflag="\$(ADD) CPU=\$(CPU_TYPE) STRSECT=CODE DEF __USE_SYSBASE=1 DEF __builtin_memcmp=memcmp DEF AMISSL DEF AMISSL_COMPILE DEF B_ENDIAN DEF THIRTY_TWO_BITS DEF OPENSSL_NO_FP_API DEF OPENSSL_NO_ENGINE IGN=55+72+154+161+304+306 NOSTKCHK STRMERGE ABSFUNCPOINTER IDLEN=100 MATH=STANDARD IDIR NetInclude: IDIR AmiSSL:include DEBUG=LINE";
+# OPT OPTGO OPTPEEP OPTLOOP OPTINL OPTINLOCAL OPTDEP=4 OPTSCHED
 $obj='.o';
 $ofile='OBJNAME ';
-$compile='';
 $define='DEF ';
 $include='IDIR ';
 
 # EXE linking stuff
 $link='${CC} LINK';
 # either SAS bsearch is fubar'd or SSLeay doesn't like it...
-$lflags='${CFLAG} NOICONS SMALLCODE "LINKOPTS=DEFINE _bsearch=_OBJ_bsearch"';
+$lflags='${CFLAG} NOICONS SMALLCODE "LINKOPTS=DEFINE _bsearch=_OBJ_bsearch LIB lib:scm.lib lib:miami.lib AmiSSL:lib/amisslauto.lib"';
 $efile='TO ';
 $exep='';
-$ex_libs="NetLib:miami.lib AmiSSL:obj/mystdio.o";
+$ex_libs="";
 
 # static library stuff
 $mklib='oml';
@@ -72,7 +73,7 @@ sub do_link_rule
 	$file =~ s/\//$o/g if $o ne '/';
 	$n=&bname($target);
 	$ret.="$target: $files $dep_libs\n";
-	$ret.="\t\$(LINK) ${efile}$target \$(LFLAGS) FROM $files LIB $libs\n\n";
+	$ret.="\t\$(LINK) ${efile}$target \$(LFLAGS) FROM $files\n\n";
 	return($ret);
 	}
 
