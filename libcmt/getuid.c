76905fc48dda883b9e51028e572e986571d0687d
Andrija Antonijevic <antony@heightanxiety.com>
2005-01-31 01:37:28 +0000

    Added OS4 code.

--- libcmt/getuid.c
+++ libcmt/getuid.c
@@ -1,18 +1,33 @@
 #include <proto/exec.h>
-#include <clib/usergroup_protos.h>
-#include <pragmas/usergroup_pragmas.h>
 
+#define PRAGMAS_USERGROUP_H
+#define INLINE4_USERGROUP_H
+#include <proto/usergroup.h>
+
+#ifndef __amigaos4__
 #pragma libcall UserGroupBase _getuid 30 00
 long _getuid(void);
+#endif
 
 long getuid(void)
 {
 	struct Library *UserGroupBase;
+#ifdef __amigaos4__
+	struct UserGroupIFace *IUserGroup;
+#endif
 	long ret = 0;
 
 	if ((UserGroupBase = OpenLibrary("usergroup.library", 1)) || (UserGroupBase = OpenLibrary("AmiTCP:Libs/usergroup.library", 1)))
 	{
+#ifndef __amigaos4__
 		ret = _getuid();
+#else
+		if (IUserGroup = (struct UserGroupIFace *)GetInterface(UserGroupBase, "main", 1, NULL))
+		{
+			ret = IUserGroup->getuid();
+			DropInterface((struct Interface *)IUserGroup);
+		}
+#endif
 		CloseLibrary(UserGroupBase);
 	}
 
