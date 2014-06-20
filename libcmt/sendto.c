c0674406e26b0d6a8ae0be547ecfb0843b0e7b3a
Thore Böckelmann <tboeckel@gmx.de>
2014-06-20 10:30:48 +0000

    * libcmt: added implementations of recvfrom() and sendto().

---
+++ libcmt/sendto.c
@@ -0,0 +1,52 @@
+#include <sys/types.h>
+#include <netinet/in.h>
+
+#ifdef __amigaos4__
+#undef __USE_INLINE__
+#include <proto/bsdsocket.h>
+#else
+#define AMITCP_NEW_NAMES
+#include <errno.h>
+#include "multitcp.h"
+#include <internal/amissl.h>
+#endif
+
+#include "libcmt.h"
+
+long
+sendto(
+     long s,
+     const void *buf,
+     long len,
+     long flags,
+     const struct sockaddr *to,
+     long tolen)
+{
+#ifdef __amigaos4__
+  GETISOCKET();
+  if(ISocket) return ISocket->sendto(s,(void *)buf,len,flags,to,tolen);
+  else return -1;
+#else
+	GETSTATE();
+
+	if (state->SocketBase)
+	{
+		switch(state->TCPIPStackType)
+		{
+			case TCPIP_Miami:
+			case TCPIP_AmiTCP:
+			case TCPIP_MLink:
+				return amitcp_SendTo(s,(void *)buf,len,flags,to,tolen);
+				break;
+			case TCPIP_IN225:
+				return in225_sendto(s,(void *)buf,len,flags,to,tolen);
+				break;
+			case TCPIP_Termite:
+				return termite_sendto(s,(void *)buf,len,flags,to,tolen);
+				break;
+		}
+	}
+
+	return(-1);
+#endif
+}
