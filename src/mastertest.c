b1d7587dfba17cc48c838ab5adb13f8300c9b4b9
Thore Böckelmann <tboeckel@gmx.de>
2014-11-27 15:09:24 +0000

    * src/mastertest.c: print out the obtained library bases before doing the actual tests.

--- src/mastertest.c
+++ src/mastertest.c
@@ -7,23 +7,43 @@ int main(void)
 {
 	struct Library *AmiSSLMasterBase;
 
-#if 0
+#if 1
+{
+	printf("single base test\n");
+	AmiSSLMasterBase = OpenLibrary("amisslmaster.library", 0);
+	printf("base %08lx\n", AmiSSLMasterBase);
+	if(AmiSSLMasterBase != NULL)
+	{
+		printf("checking base %08lx...", AmiSSLMasterBase);
+		InitAmiSSLMaster(AMISSL_V097g, TRUE);
+		printf(" done\n");
+		CloseLibrary(AmiSSLMasterBase);
+	}
+	printf("single base test finished\n");
+}
+#endif
+
+#if 1
+{
 	struct Library *base1;
 	struct Library *base2;
 
+	printf("multi base test\n");
 	base1 = OpenLibrary("amisslmaster.library", 0);
+	printf("base1 %08lx\n", base1);
 	base2 = OpenLibrary("amisslmaster.library", 0);
+	printf("base2 %08lx\n", base2);
 
 	if(base1 != NULL && base2 != NULL)
 	{
 		AmiSSLMasterBase = base1;
-		printf("checking base %08lx...", AmiSSLMasterBase); kprintf("checking base %08lx...\n", AmiSSLMasterBase);
-		InitAmiSSLMaster(AMISSL_V097g, TRUE); kprintf(" done\n");
+		printf("checking base %08lx...", AmiSSLMasterBase);
+		InitAmiSSLMaster(AMISSL_V097g, TRUE);
 		printf(" done\n");
 
 		AmiSSLMasterBase = base2;
-		printf("checking base %08lx...", AmiSSLMasterBase); kprintf("checking base %08lx...\n", AmiSSLMasterBase);
-		InitAmiSSLMaster(AMISSL_V2, TRUE); kprintf(" done\n");
+		printf("checking base %08lx...", AmiSSLMasterBase);
+		InitAmiSSLMaster(AMISSL_V2, FALSE);
 		printf(" done\n");
 	}
 
@@ -32,16 +52,8 @@ int main(void)
 
 	if(base2 != NULL)
 		CloseLibrary(base2);
-#endif
-
-#if 1
-	if((AmiSSLMasterBase = OpenLibrary("amisslmaster.library", 0)) != NULL)
-	{
-		printf("checking base %08lx...", AmiSSLMasterBase); kprintf("checking base %08lx...\n", AmiSSLMasterBase);
-		InitAmiSSLMaster(AMISSL_V097g, TRUE);
-		printf(" done\n"); kprintf(" done\n");
-		CloseLibrary(AmiSSLMasterBase);
-	}
+	printf("multi base test finished\n");
+}
 #endif
 
 	return 0;
