080482b20d6a640520257e9c06ef83fb7ba0bbdf
Thore Böckelmann <tboeckel@gmx.de>
2015-08-07 07:35:19 +0000

    * src/amissl_libinit.c: added an almost complete function table.

--- src/amissl_lib_protos.h
+++ src/amissl_lib_protos.h
@@ -16,12 +16,6 @@
 #endif
 #define __BASE_OR_IFACE			__BASE_OR_IFACE_TYPE __BASE_OR_IFACE_VAR
 
-struct AmiSSLInitStruct;
-struct TagItem;
-LIBPROTO(InternalInitAmiSSL, void, REG(a6, UNUSED __BASE_OR_IFACE), REG(a0, struct AmiSSLInitStruct * amisslinit));
-LIBPROTO(InitAmiSSLA, long, REG(a6, UNUSED __BASE_OR_IFACE), REG(a0, struct TagItem * tagList));
-LIBPROTO(CleanupAmiSSLA, long, REG(a6, UNUSED __BASE_OR_IFACE), REG(a0, struct TagItem * tagList));
-
 #ifdef __amigaos4__
 LIBPROTOVA(InitAmiSSL, long, REG(a6, UNUSED __BASE_OR_IFACE), ...);
 LIBPROTOVA(CleanupAmiSSL, long, REG(a6, UNUSED __BASE_OR_IFACE), ...);
