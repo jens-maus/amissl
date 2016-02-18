0c1e9b3ac991ff5ee58589eff5bfdb7d04ae59e2
Jens Maus <mail@jens-maus.de>
2016-02-18 16:39:55 +0100

    updated all root-CA files using the new "update-cert.sh" script.

---
+++ certs/update-certs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# script to update the root CA files in this
+# directory
+#
+
+CURDIR=$(pwd)
+TMPDIR=$(mktemp -d)
+cd ${TMPDIR}
+wget -O ca-bundle.crt https://curl.haxx.se/ca/cacert.pem
+awk 'BEGIN {c=0} v{v=v"\n"$0} /----BEGIN/{v=$0;c++}/----END/&&v { print v > "cert." c ".pem"; v=x}' ca-bundle.crt
+c_rehash .
+rm -f ${CURDIR}/*.[[:alnum:]]
+find . -type l -exec cp {} ${CURDIR}/ \;
+rm -rf ${TMPDIR}
