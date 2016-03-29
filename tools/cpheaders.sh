7d0544f09f499fb8f43c871c6e4139771a48dc62
Jens Maus <mail@jens-maus.de>
2016-03-29 12:51:57 +0200

    the internal crypto header files don't need to be patched and thus cpheaders.sh can be modified accordingly.

--- tools/cpheaders.sh
+++ tools/cpheaders.sh
@@ -9,13 +9,3 @@ for header in ${BUILD_D}/openssl/include/openssl/*.h openssl/include/openssl/*.h
     echo "copied+patched ${header} to include/openssl"
   fi
 done
-mkdir -p ${BUILD_D}/crypto/include/internal
-rm -f ${BUILD_D}/cryptol/include/internal/*.h
-for header in openssl/crypto/include/internal/*.h; do
-  file=$(basename ${header})
-  if echo "${file}" | grep -qv "__"; then
-    cp -a ${header} ${BUILD_D}/crypto/include/internal/
-    sed -i '1s/^/#if !defined(PROTO_AMISSL_H) \&\& !defined(AMISSL_COMPILE)\n#include <proto\/amissl.h>\n#endif\n/' ${BUILD_D}/crypto/include/internal/${file}
-    echo "copied+patched ${header} ${BUILD_D}/crypto/include/internal"
-  fi
-done
