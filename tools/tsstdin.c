453432a4b15e48556128f1f8720ec1dc54588c0f
Thore Böckelmann <tboeckel@gmx.de>
2017-02-13 16:13:08 +0100

    * tools/tsstdin.c: added source of "timestamp standard input" tool. It reads each line from stdin, prepends it with the elapsed time since program start and outputs everything to stdout again.

---
+++ tools/tsstdin.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <errno.h>
+
+#if defined(__amigaos3__)
+// clib2 is lacking all these functions
+#ifndef timersub
+# define timersub(a, b, result)                                               \
+  do {                                                                        \
+    (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;                             \
+    (result)->tv_usec = (a)->tv_usec - (b)->tv_usec;                          \
+    if ((result)->tv_usec < 0) {                                              \
+      --(result)->tv_sec;                                                     \
+      (result)->tv_usec += 1000000;                                           \
+    }                                                                         \
+  } while (0)
+#endif
+
+#ifndef getdelim
+#define MIN_LINE_SIZE 4
+#define DEFAULT_LINE_SIZE 128
+
+ssize_t getdelim(char **bufptr, size_t *n, int delim, FILE *fp)
+{
+  char *buf;
+  char *ptr;
+  size_t newsize, numbytes;
+  int pos;
+  int ch;
+  int cont;
+
+  if (fp == NULL || bufptr == NULL || n == NULL)
+    {
+      errno = EINVAL;
+      return -1;
+    }
+
+  buf = *bufptr;
+  if (buf == NULL || *n < MIN_LINE_SIZE)
+    {
+      buf = (char *)realloc (*bufptr, DEFAULT_LINE_SIZE);
+      if (buf == NULL)
+        {
+	  return -1;
+        }
+      *bufptr = buf;
+      *n = DEFAULT_LINE_SIZE;
+    }
+
+  flockfile (fp);
+
+  numbytes = *n;
+  ptr = buf;
+
+  cont = 1;
+
+  while (cont)
+    {
+      /* fill buffer - leaving room for nul-terminator */
+      while (--numbytes > 0)
+        {
+          if ((ch = getc_unlocked (fp)) == EOF)
+            {
+	      cont = 0;
+              break;
+            }
+	  else
+            {
+              *ptr++ = ch;
+              if (ch == delim)
+                {
+                  cont = 0;
+                  break;
+                }
+            }
+        }
+
+      if (cont)
+        {
+          /* Buffer is too small so reallocate a larger buffer.  */
+          pos = ptr - buf;
+          newsize = (*n << 1);
+          buf = realloc (buf, newsize);
+          if (buf == NULL)
+            {
+              cont = 0;
+              break;
+            }
+
+          /* After reallocating, continue in new buffer */
+          *bufptr = buf;
+          *n = newsize;
+          ptr = buf + pos;
+          numbytes = newsize - pos;
+        }
+    }
+
+  funlockfile (fp);
+
+  /* if no input data, return failure */
+  if (ptr == buf)
+    return -1;
+
+  /* otherwise, nul-terminate and return number of bytes read */
+  *ptr = '\0';
+  return (ssize_t)(ptr - buf);
+}
+#endif
+
+#ifndef getline
+ssize_t getline(char **lptr, size_t *n, FILE *fp)
+{
+  return getdelim (lptr, n, '\n', fp);
+}
+#endif
+#endif // __amigaos3__
+
+int main(int argc, char **argv)
+{
+	char *line = NULL;
+	size_t linesize = 0;
+	struct timeval start;
+
+	gettimeofday(&start, NULL);
+
+	while(getline(&line, &linesize, stdin) != -1)
+	{
+		struct timeval now;
+		struct timeval diff;
+
+		gettimeofday(&now, NULL);
+		timersub(&now, &start, &diff);
+
+		fprintf(stdout, "[%8ld.%06ld] %s", diff.tv_sec, diff.tv_usec, line);
+	}
+
+	free(line);
+
+	return 0;
+}
