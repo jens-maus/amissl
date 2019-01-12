7be8d10d712378b63befca0ca2131df9988bee37
Futaura <oliver@futaura.co.uk>
2019-01-12 10:37:17 +0000

    fixed BIO file for Amiga (BIO_METHOD struct extended)

--- openssl/crypto/bio/bss_file_amiga.c
+++ openssl/crypto/bio/bss_file_amiga.c
@@ -28,7 +28,11 @@ static const BIO_METHOD methods_filep =
 {
 	BIO_TYPE_FILE,
 	"FILE pointer (Amiga)",
+	/* TODO: Convert to new style write function */
+	bwrite_conv,
 	file_write,
+	/* TODO: Convert to new style read function */
+	bread_conv,
 	file_read,
 	file_puts,
 	file_gets,
