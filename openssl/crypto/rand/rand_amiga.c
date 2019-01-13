f0704530b134f891bb1b84f607c4a2f6f5600db2
Futaura <oliver@futaura.co.uk>
2019-01-13 14:13:05 +0000

    new-style rand_amiga OpenSSL module fixed for multi-tasking and tested

--- openssl/crypto/rand/rand_amiga.c
+++ openssl/crypto/rand/rand_amiga.c
@@ -6,6 +6,7 @@
 #ifdef OPENSSL_SYS_AMIGA
 
 #include <proto/exec.h>
+#include <proto/dos.h>
 #include <proto/timer.h>
 #include <devices/timer.h>
 #include <timeval.h>
@@ -32,127 +33,214 @@
 #endif
 #define DeleteIORequest(ioReq) FreeSysObject(ASOT_IOREQUEST,ioReq)
 
+#define DECL_TIMER_INTERFACE() struct TimerIFace *ITimer = GlobalTimer.ITimer
+
 #else
 
 #define GetInterface(a, b, c, d) 1
 #define DropInterface(x)
 
+#define DECL_TIMER_INTERFACE() struct Device *TimerBase = GlobalTimer.TimerBase
+
 #endif /* !__amigaos4__ */
 
-static struct MsgPort *port;
-static struct TimeRequest *time_request;
+/* Global timer, for use only by rand_pool_add_nonce_data() and
+** rand_pool_add_additional_data() - timer.device function calls
+** only (request must not be used in a messaging context!)
+*/
+static struct {
+	struct TimeRequest *time_request;
+	#if defined(__amigaos4__)
+	struct TimerIFace *ITimer;
+	#else
+	struct Device *TimerBase;
+	#endif
+} GlobalTimer;
 
 #if defined(__amigaos4__)
-static struct IOStdReq *entropy_request;
-struct TimerIFace *ITimer;
-#else
-struct Device *TimerBase;
-#endif
-
-size_t rand_pool_acquire_entropy(RAND_POOL *pool)
+static size_t read_entropy(RAND_POOL *pool, size_t bytes_needed, struct MsgPort *port)
 {
 	unsigned char temp_buffer[SHA_DIGEST_LENGTH], data_buffer[SHA_DIGEST_LENGTH];
-	size_t bytes_needed = rand_pool_bytes_needed(pool, 1 /*entropy_factor*/);
+	struct IOStdReq *entropy_request;
 
-#if defined(__amigaos4__)
-	while(bytes_needed > 0)
+	if ((entropy_request = (struct IOStdReq *)CreateIORequest(port, sizeof(*entropy_request))))
 	{
-		entropy_request->io_Command = TR_READENTROPY;
-		entropy_request->io_Data = &temp_buffer[0];
-		entropy_request->io_Length = sizeof(temp_buffer);
+		if (OpenDevice(TIMERNAME, UNIT_ENTROPY, (struct IORequest *)entropy_request, 0) == 0)
+		{
+			while(bytes_needed > 0)
+			{
+				entropy_request->io_Command = TR_READENTROPY;
+				entropy_request->io_Data = &temp_buffer[0];
+				entropy_request->io_Length = sizeof(temp_buffer);
 
-		if (DoIO((struct IORequest *)entropy_request) == 0)
-		{
-			size_t bytes_read = entropy_request->io_Actual;
-			if (bytes_read > bytes_needed) bytes_read = bytes_needed;
-			SHA1(&temp_buffer[0], sizeof(temp_buffer), &data_buffer[0]);
-			rand_pool_add(pool, &data_buffer[0], bytes_read, 8 * bytes_read);
-			bytes_needed -= bytes_read;
+				if (DoIO((struct IORequest *)entropy_request) == 0)
+				{
+					size_t bytes_read = entropy_request->io_Actual;
+					if (bytes_read > bytes_needed) bytes_read = bytes_needed;
+					SHA1(&temp_buffer[0], sizeof(temp_buffer), &data_buffer[0]);
+					rand_pool_add(pool, &data_buffer[0], bytes_read, 8 * bytes_read);
+					bytes_needed -= bytes_read;
+				}
+				else
+				{
+					break;
+				}
+			}
+			CloseDevice((struct IORequest *)entropy_request);
 		}
-		else
-		{
-			break;
-		}
+		DeleteIORequest((struct IORequest *)entropy_request);
 	}
+        return bytes_needed;
+}
 #endif /* __amigaos4__ */
 
-	/* The following block will be used on "classic" machines. It does not generate
-	 * a high degree of randomness, but it does the job since RAND_poll is
-	 * called only once by OpenSSL to generate a 32 byte seed.
-	 */
-	if (bytes_needed > 0)
+/*
+** This must be re-entrant as may be called by multiple tasks hence must
+** always allocate our own port and requests and not use the global timer
+*/
+size_t rand_pool_acquire_entropy(RAND_POOL *pool)
+{
+	struct TimeRequest *time_request;
+	struct MsgPort *port = NULL;
+	size_t bytes_needed = rand_pool_bytes_needed(pool, 1 /*entropy_factor*/);
+
+	if ((bytes_needed > 0) && (port = CreateMsgPort()))
 	{
-		struct EClockVal curr_eclock;
-		ULONG prev_ev_lo = 0;
-		struct TimeVal tv;
-		int i, attempt;
-		BOOL aborted;
-
-		ReadEClock(&curr_eclock);
-		aborted = FALSE;
-
-		while(!aborted && bytes_needed > 0)
-		{
-			for(i = 0;
-			    !aborted && i < (int)sizeof(temp_buffer) - (int)sizeof(ULONG);
-			    i++)
-			{
-				attempt = 0;
-
-				/* Ask for a one microsecond delay and measure the time
-				 * the delay actually took.
-				 */
-				do
-				{
-					time_request->Request.io_Command = TR_ADDREQUEST;
-					time_request->Time.Seconds = 0;
-					time_request->Time.Microseconds = 1;
-
-					if (DoIO((struct IORequest *)time_request) == 0)
-					{
-						prev_ev_lo = curr_eclock.ev_lo;
-						ReadEClock(&curr_eclock);
-
-						attempt++;
-					}
-					else
-						aborted = TRUE;
-				} while(!aborted && prev_ev_lo == 0 && attempt < MAX_ATTEMPTS);
-
-				if (attempt >= MAX_ATTEMPTS)
-					aborted = TRUE;
-
-				/* Since we are going for randomness, ev_hi is irrelevant */
-				temp_buffer[i] = (unsigned char)(curr_eclock.ev_lo - prev_ev_lo);
-			}
-
-			GetSysTime(TIMEVAL(&tv));
-
-			if (sizeof(temp_buffer) > sizeof(ULONG))
-				*(ULONG *)&temp_buffer[sizeof(temp_buffer) - sizeof(ULONG)]
-				    = tv.Microseconds;
-
-			/* Shuffle the bits around and specify that about
-			 * one fourth of it adds to the entropy.
-			 */
-			if (!aborted)
-			{
-				size_t bytes_read = sizeof(data_buffer) / 4;
-				if (bytes_read > bytes_needed) bytes_read = bytes_needed;
-				SHA1(&temp_buffer[0], sizeof(temp_buffer), &data_buffer[0]);
-				rand_pool_add(pool, &data_buffer[0], bytes_read, 8 * bytes_read);
-				bytes_needed -= bytes_read;
-			}
-		}
+#if defined(__amigaos4__)
+		bytes_needed = read_entropy(pool,bytes_needed,port);
+#endif
 	}
+
+	/* The following block will be used on "classic" machines. It does not generate
+	 * a high degree of randomness, but it does the job since rand_pool_acquire_entropy
+	 * is usually called infrequently by OpenSSL to generate a 32 byte seed.
+	 */
+	if ((bytes_needed > 0) && port &&
+	    (time_request = (struct TimeRequest *)CreateIORequest(port, sizeof(*time_request))))
+	{
+		if (OpenDevice(TIMERNAME, UNIT_VBLANK, &time_request->Request, 0) == 0)
+		{
+			#if defined(__amigaos4__)
+			struct TimerIFace *ITimer;
+			if (time_request->Request.io_Device &&
+			    (ITimer = (struct TimerIFace *)GetInterface(&time_request->Request.io_Device->dd_Library, "main", 1, NULL)))
+			#else
+			struct Device *TimerBase;
+			if ((TimerBase = time_request->Request.io_Device))
+			#endif
+			{
+				unsigned char temp_buffer[SHA_DIGEST_LENGTH], data_buffer[SHA_DIGEST_LENGTH];
+				struct EClockVal curr_eclock;
+				ULONG prev_ev_lo = 0;
+				struct TimeVal tv;
+				int i, attempt;
+				BOOL aborted;
+
+				ReadEClock(&curr_eclock);
+				aborted = FALSE;
+
+				while(!aborted && bytes_needed > 0)
+				{
+					for(i = 0;
+					    !aborted && i < (int)sizeof(temp_buffer) - (int)sizeof(ULONG);
+					    i++)
+					{
+						attempt = 0;
+
+						/* Ask for a one microsecond delay and measure the time
+						 * the delay actually took.
+						 */
+						do
+						{
+							time_request->Request.io_Command = TR_ADDREQUEST;
+							time_request->Time.Seconds = 0;
+							time_request->Time.Microseconds = 1;
+
+							if (DoIO((struct IORequest *)time_request) == 0)
+							{
+								prev_ev_lo = curr_eclock.ev_lo;
+								ReadEClock(&curr_eclock);
+
+								attempt++;
+							}
+							else
+								aborted = TRUE;
+						} while(!aborted && prev_ev_lo == 0 && attempt < MAX_ATTEMPTS);
+
+						if (attempt >= MAX_ATTEMPTS)
+							aborted = TRUE;
+
+						/* Since we are going for randomness, ev_hi is irrelevant */
+						temp_buffer[i] = (unsigned char)(curr_eclock.ev_lo - prev_ev_lo);
+					}
+
+					GetSysTime(TIMEVAL(&tv));
+
+					if (sizeof(temp_buffer) > sizeof(ULONG))
+						*(ULONG *)&temp_buffer[sizeof(temp_buffer) - sizeof(ULONG)]
+						    = tv.Microseconds;
+
+					/* Shuffle the bits around and specify that about
+					 * one fourth of it adds to the entropy.
+					 */
+					if (!aborted)
+					{
+						size_t bytes_read = sizeof(data_buffer) / 4;
+						if (bytes_read > bytes_needed) bytes_read = bytes_needed;
+						SHA1(&temp_buffer[0], sizeof(temp_buffer), &data_buffer[0]);
+						rand_pool_add(pool, &data_buffer[0], bytes_read, 8 * bytes_read);
+						bytes_needed -= bytes_read;
+					}
+				}
+				#if defined(__amigaos4__)
+				DropInterface((struct Interface *)ITimer);
+				#endif
+			}
+			CloseDevice(&time_request->Request);
+		}
+		DeleteIORequest(&time_request->Request);
+	}
+
+	DeleteMsgPort(port);
 
 	return rand_pool_entropy_available(pool);
 }
 
+/*
+** As getpid() and CRYPTO_THREAD_get_current_id() both result in
+** FindTask(NULL), we need to get something different if possible.
+*/
+static uint32_t amiga_get_process_id(void)
+{
+	struct Task *task = FindTask(NULL);
+	uint32_t ret;
+
+	if (task->tc_Node.ln_Type == NT_PROCESS)
+	{
+#if defined(__amigaos4__)
+		ret = (uint32_t)((struct Process *)task)->pr_ProcessID;
+#else
+		ret = (uint32_t)((struct Process *)task)->pr_SegList;
+		if (!ret)
+		{
+			ret = (uint32_t)task;
+		}
+#endif
+	}
+	else
+	{
+		ret = (uint32_t)task;
+	}
+
+	return ret;
+}
+
 int rand_pool_add_nonce_data(RAND_POOL *pool)
 {
+	DECL_TIMER_INTERFACE();
+
 	struct {
-		struct Task *pid;
+		uint32_t pid;
 		CRYPTO_THREAD_ID tid;
 		struct TimeVal tv;
 	} data = { 0 };
@@ -162,15 +250,20 @@ int rand_pool_add_nonce_data(RAND_POOL *pool)
 	 * ensure that the nonce is unique whith high probability for
 	 * different process instances.
 	 */
-	data.pid = FindTask(NULL);
+	data.pid = amiga_get_process_id();
 	data.tid = CRYPTO_THREAD_get_current_id();
 	GetSysTime(TIMEVAL(&data.tv));
 
 	return rand_pool_add(pool, (unsigned char *)&data, sizeof(data), 0);
 }
 
+/*
+** Frequently called, so must have low overhead
+*/
 int rand_pool_add_additional_data(RAND_POOL *pool)
 {
+	DECL_TIMER_INTERFACE();
+
 	struct {
 		CRYPTO_THREAD_ID tid;
 		struct EClockVal curr_eclock;
@@ -189,37 +282,30 @@ int rand_pool_add_additional_data(RAND_POOL *pool)
 
 int rand_pool_init(void)
 {
+	struct MsgPort *port;
 	if ((port = CreateMsgPort()))
 	{
-		if ((time_request = (struct TimeRequest *)CreateIORequest(port, sizeof(*time_request))))
+		if ((GlobalTimer.time_request = (struct TimeRequest *)CreateIORequest(port, sizeof(*GlobalTimer.time_request))))
 		{
-			if (OpenDevice(TIMERNAME, UNIT_VBLANK, (struct IORequest *)time_request, 0) == 0)
+			if (OpenDevice(TIMERNAME, UNIT_VBLANK, &GlobalTimer.time_request->Request, 0) == 0)
 			{
 #if defined(__amigaos4__)
-				if (time_request->Request.io_Device)
+				if (GlobalTimer.time_request->Request.io_Device)
 				{
-					if ((ITimer = (struct TimerIFace *)GetInterface(&time_request->Request.io_Device->dd_Library, "main", 1, NULL)))
+					if ((GlobalTimer.ITimer = (struct TimerIFace *)GetInterface(&GlobalTimer.time_request->Request.io_Device->dd_Library, "main", 1, NULL)))
 					{
-						if ((entropy_request = (struct IOStdReq *)CreateIORequest(port, sizeof(*entropy_request))))
-						{
-							if (OpenDevice(TIMERNAME, UNIT_ENTROPY, (struct IORequest *)entropy_request, 0) == 0)
-							{
-								return 1;
-							}
-							DeleteIORequest((struct IORequest *)entropy_request);
-						}
-						DropInterface((struct Interface *)ITimer);
+						return 1;
 					}
 				}
 #else
-				if ((TimerBase = time_request->Request.io_Device))
+				if ((GlobalTimer.TimerBase = GlobalTimer.time_request->Request.io_Device))
 				{
 					return 1;
 				}
 #endif
-				CloseDevice(&time_request->Request);
+				CloseDevice(&GlobalTimer.time_request->Request);
 			}
-			DeleteIORequest(&time_request->Request);
+			DeleteIORequest(&GlobalTimer.time_request->Request);
 		}
 		DeleteMsgPort(port);
 	}
@@ -228,13 +314,12 @@ int rand_pool_init(void)
 
 void rand_pool_cleanup(void)
 {
+        struct MsgPort *port = GlobalTimer.time_request->Request.io_Message.mn_ReplyPort;
 #if defined(__amigaos4__)
-	CloseDevice((struct IORequest *)entropy_request);
-	DeleteIORequest((struct IORequest *)entropy_request);
-	DropInterface((struct Interface *)ITimer);
+	DropInterface((struct Interface *)GlobalTimer.ITimer);
 #endif
-	CloseDevice(&time_request->Request);
-	DeleteIORequest(&time_request->Request);
+	CloseDevice(&GlobalTimer.time_request->Request);
+	DeleteIORequest(&GlobalTimer.time_request->Request);
 	DeleteMsgPort(port);
 }
 
