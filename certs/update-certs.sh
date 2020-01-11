a1cecdcc9a35d5ad5c79939b9d03c288096ead1b
Futaura <oliver@futaura.co.uk>
2020-01-11 18:26:22 +0000

    Updated certs with latest, and fixed a couple of old ones #36

--- certs/update-certs.sh
+++ certs/update-certs.sh
@@ -9,6 +9,7 @@ TMPDIR=$(mktemp -d)
 cd ${TMPDIR}
 wget -O ca-bundle.crt https://curl.haxx.se/ca/cacert.pem
 awk 'BEGIN {c=0} v{v=v"\n"$0} /----BEGIN/{v=$0;c++}/----END/&&v { print v > "cert." c ".pem"; v=x}' ca-bundle.crt
+rm ca-bundle.crt
 c_rehash .
 rm -f ${CURDIR}/*.[[:alnum:]]
 find . -type l -exec cp {} ${CURDIR}/ \;
