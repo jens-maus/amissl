76a3486f0a45dfb31c1877eac44db6836dd5c491
Futaura <oliver@futaura.co.uk>
2022-02-26 18:32:39 +0000

    Make "#pragma once" usage conditional, based on compiler

--- tools/cpheaders.sh
+++ tools/cpheaders.sh
@@ -6,6 +6,7 @@ for header in ${BUILD_D}/openssl/include/openssl/*.h openssl/include/openssl/*.h
   if echo "${file}" | grep -qv "__"; then
     cp -a ${header} include/openssl/
     sed -i '1s/^/#if !defined(PROTO_AMISSL_H) \&\& !defined(AMISSL_COMPILE)\n#include <proto\/amissl.h>\n#endif\n/' include/openssl/${file}
+    sed -i 's/\#\s*pragma once/# if defined(__GNUC__) \&\& (__GNUC__ > 3 || (__GNUC__ == 3 \&\& __GNUC_MINOR__ > 3))\n&\n# endif/' include/openssl/${file}
     echo "copied+patched ${header} to include/openssl"
   fi
 done
