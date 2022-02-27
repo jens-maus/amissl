e2342fbefdc1206531cb61762ec6cacdb72c1bc1
Futaura <oliver@futaura.co.uk>
2022-02-27 15:14:46 +0000

    Added off_t typedef for SAS/C and VBCC

--- include/amissl/types.h
+++ include/amissl/types.h
@@ -32,10 +32,17 @@ typedef int int32_t;
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
+typedef long int off_t;
 
 /* Cannot use 64-bit functions - used only to allow headers to be parsed */
 typedef int int64_t;
 typedef unsigned int uint64_t;
+
+#elif defined(__VBCC__)
+
+typedef long int off_t;
+
+#include <stdint.h>
 
 #else
 
