f230aa4feeea6e27f05e0a00eb6aa1231370d995
Futaura <oliver@futaura.co.uk>
2022-10-11 21:30:40 +0100

    Fix up OpenSSL.doc version string

--- tools/openssldoc.sh
+++ tools/openssldoc.sh
@@ -27,8 +27,8 @@
 # Read the OpenSSL .pod manual files and output them in OpenSSL.doc format
 
 releasever=`grep ^VERSION= Makefile | awk -F= '{ print $2 }'`
-releaserev=`grep ^AMISSLMASTERREVISION= Makefile | awk -F= '{ print $2 }'`
-releasedate=`grep ^AMISSLMASTERDATE= Makefile | awk -F= '{ print $2 }'`
+releaserev=`grep ^REVISION= Makefile | awk -F= '{ print $2 }'`
+releasedate=`date +%-d.%m.%Y`
 
 printf "\$VER: OpenSSL.doc $releasever.$releaserev ($releasedate)\n\n"
 
